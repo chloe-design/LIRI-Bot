fb410c6441f6e2548af4a74673ecdab8b1ba93fb
Chloe Stephens <chloerstephens@gmail.com>
2019-12-12 23:06:54 -0700

    Changes to liri.js to ensure functionality

--- liri.js
+++ liri.js
@@ -16,3 +16,28 @@ var moment = require("moment");
 //Spotify Keys
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+
+inquirer.prompt([
+    {
+        type: 'key in',
+        name: 'call'
+        message: 'Enter in a request from: concert this, spotify this song, movie this and do what it says'
+    }
+
+
+]).then(function (response) {
+    switch (response.call) {
+        case "Search for Concert":
+            searchforconcert();
+            break;
+        case "Search for Song":
+            spotify();
+            break;
+        case "Search for a Movie":
+            movie();
+            break;
+        case "Do what Says":
+            dowhatitsays();
+            break;
+    }
+})
